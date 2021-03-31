8231e21edd4c204f84c3a4c2408ab5225b3094f4
crackalamoo <37599247+crackalamoo@users.noreply.github.com>
2021-03-31 05:25:44 -0400

    Spanish and Hindi translations

---
+++ lang/en.js
@@ -0,0 +1,197 @@
+langData["en"] = {
+  "begin": "Begin by creating categories for grades. If the class is based on point system, make a category called something like \"Total\" and consider it 100% of your grade.",
+  "catInstruct": "If the class is based on point system, make a category " +
+    "called something like \"Total\" and consider it 100% of your grade. " +
+    "Please don't make a category with an existing name, as I am too " +
+    "lazy to make sure you don't do so.",
+  "catList": "<strong>Click a category</strong> to add or remove assignments and to see that category's score.<br><br>Categories:<br>",
+  "catAvg": "Average in $CATEGORY: $NUMBER%",
+  "avg": "Score so far in this class: $SCORE% (Letter grade: $LETTER)<br>" +
+    "Unweighted GPA: $GPA<br>Sum of all categories: $SUM% (Should be 100%)<br>",
+  "noCat": "No category is currently selected",
+  "selCat": "Score so far in $CATEGORY: $SCORE%",
+  "scoreNaN": "You have a denominator of zero or another error in at least one category. Try adding some assignments.",
+  "rampalInstruct": "<b>Do the following for <em>each</em> assignment:</b><br>" +
+    "Click on the assignment<br><img src=\"rampal/rampal1.png\"><br><img src=\"rampal/rampal2.png\"><br>Look at the <strong>\"Count as\"</strong> and enter it above",
+  "catsFound": "$NUMBER categories found: $CATEGORIES",
+  "pointCheck": "This class is point system",
+  "next": "Next",
+  "back": "Back",
+  "done": "Done",
+  "continue": "Continue",
+  "help": "Help",
+  "submit": "Submit",
+  "apply": "Apply",
+  "add": "Add",
+  "delete": "Delete",
+  "edit": "Edit grades",
+  "newClass": "New class (automatic)",
+  "notPossibleGrade": "Unfortunately this grade is not possible for you this semester. Good luck for next semester!",
+  "minGrade": "Minimum grade required on semester exam: $MIN% (goes in as $LETTER/$NUMBER%)",
+  "fritzExam": "Minimum grade required on semester exam: $MIN% (I hope you don't Fritz the exam that badly)",
+  "validData": "Please enter valid data.",
+  "mailSent": "Your response was sent and the class should be available shortly. Thank you!" +
+    "<br>For now, go back and enter weightings manually.<br>If the class isn't available within about three days, please contact" +
+    "me at bd542591@ahschool.com; I may be having trouble understanding your request",
+  "semGrade": "Grade for the semester: $LETTER ($NUMBER%)",
+  "gpaClass": "$INDEX: $GPA ($GRADE, $LEVEL, $CREDITS credits)",
+  "gpaSum": "<b>GPA sum: $SUM<br>Total credits: $CREDITS<br>Cumulative GPA: $GPA</b>",
+  "reqScore_0": "You must receive $PTS/$TOT to have $GRADE% in the class",
+  "reqScore": "You must receive $PTS/$TOT ($PERCENT%) to have $GRADE% in the class",
+  "setLang": "Language:&nbsp;",
+  "welcome": "Welcome to the grade calculator. First, you need to make sure you have " +
+    "JavaScript. If you don't know what that is, don't worry. I'll do it for you:",
+  "jsSuccess": "If you're reading this message, you have JavaScript and can use this page.",
+  "quarterGrades": "Quarter grades",
+  "intro": "<b>NOTE:</b> Despite my love of computers, I am also human. Please contact me at bd542591@ahschool.com if you catch a mistake." +
+    "<br><br>Begin by selecting a calculation:",
+  "semGradesButton": "Semester/Exam grades",
+  "gpaCalcButton": "GPA calculator",
+  "newCatButton": "New Category",
+  "catName": "Name:&nbsp;",
+  "catPercent": "Percentage:&nbsp;",
+  "createCat": "Create Category",
+  "addAssignment": "Add Assignment",
+  "deleteAssignment": "Delete Assignment",
+  "editInstruct": "Enter Score/Possible. Examples: 5/6.0, 8.5/9, 3/3.<br>" +
+    "To delete an assignment, enter its score <strong>in the same way</strong> and click \"Delete Assignment\".<br>" +
+    "To <strong>change the grade on an assignment</strong>, add or remove an assignment out of 0 points (ex: 5/0).",
+  "letterGrade": "Letter grade:&nbsp;",
+  "selectLetter": "Select a letter grade",
+  "whatNeed": "What do I need on an upcoming assignment?",
+  "creditCathy": "Credit to Cathy T. Quan for this idea",
+  "desiredGrade": "Grade desired in the class:&nbsp;",
+  "numPointsWorth": "Number of points the assignment is worth:&nbsp;",
+  "calcRequiredButton": "Calculate Required Score",
+  "changeWeightButton": "Change Weighting",
+  "copyGradesInstruct": "Simply copy your grades from MyBackpack as shown in the image below.",
+  "mobileCopyInstruct": "(If you are on a <strong>mobile device</strong>, including iPads, you should select <i>" +
+    "Assignment</i>, then select all the grades)",
+  "pasteGradesInstruct": "Now paste them into the box below and click <strong>Next</strong> under the box:",
+  "superAlgorithm": "My super cool algorithm simplified your grades to the following (Category, Score/Possible):",
+  "dontWorry": "<strong>If no grades have been entered yet</strong> for a category, that category may be missing from the above list." +
+    " Don't worry, you can manually add that category after the calculation. Also don't worry if this class" +
+    " is based on <strong>point system</strong> rather than categories.",
+  "shouldContinue": "Should I continue?",
+  "rampalButton": "Click here for Mr. Rampal's class",
+  "manualButton": "Manually enter grades",
+  "selClass": "<strong>Select a class</strong> to automatically fill in percentages/weighting. (Credit to <span id=\"seth\">Seth</span> Groper for this idea)",
+  "selClassOption": "Select a class",
+  "addClassOption": "Add a class to this list",
+  "sethClassInstruct": "Please enter a percentage for each category <strong><i>or</i></strong> check that this class is point system:",
+  "pointSystemInstruct": "To figure out if a class is point system or categories, check how much quizzes, homework, and tests are worth." +
+    " If they are out of about the same number of points, it is categories. Otherwise, it may be point system.",
+  "nameOfClass": "Name of class:&nbsp;",
+  "pointSystemInstruct2": "For <strong>point system</strong>, type &ldquo;point system&rdquo; for names and weights.",
+  "enterCatNames": "EXACT names of categories as they appear in MyBackpack:&nbsp;",
+  "enterWeights": "Weight of each category in above order:&nbsp;",
+  "enterNotes": "Notes (optional):&nbsp;",
+  "minExam": "Minimum exam grade required",
+  "wantAtLeast": "I want at least:&nbsp;",
+  "selectAbove": "Select grades above to calculate",
+  "semWithExam": "Semester grade with exam",
+  "ritvikCalc": "Ritvik Teegavarapu AHS GPA Calculator",
+  "ritvikHonor": "Suggested by and named in honor of Ritvik Teegavarapu",
+  "clearDataButton": "Clear GPA Data",
+  "clearDataExp": "This will force you to re-enter your GPA next time you visit this page." +
+    " This will NOT erase the data from the page right now.<br><br>" +
+    "If you leave the data rather than clearing it, everything will be saved " +
+    "next time you visit this page.",
+  "creditNum": "Credit number&nbsp;",
+  "getCreditButton": "View credit",
+  "modifyCreditHeading": "<b>Modify/delete grades</b>",
+  "confirm": "Are you sure you want to do this?"
+};
+function setLanguage(l) {
+  lang = l;
+  document.getElementById("javascript").innerHTML = langData[lang].jsSuccess;
+  langHTML("langSelect", "setLang");
+  langHTML("welcome");
+  langHTML("autoCalculation", "quarterGrades");
+  langHTML("help");
+  langHTML("intro");
+  langHTML("semGradesButton");
+  langHTML("gpaCalcButton");
+  langHTML("manualBack", "back");
+  langHTML("categoryBack", "back");
+  langHTML("autoBack", "back");
+  langHTML("faqBack", "back");
+  langHTML("semesterBack", "back");
+  langHTML("gpaBack", "back");
+  langHTML("newCatButton");
+  langHTML("catInstruct");
+  langHTML("catName");
+  langHTML("catPercent");
+  langHTML("createCat");
+  langHTML("doneEditCat", "done");
+  langHTML("editInstruct");
+  langHTML("editLetter", "letterGrade");
+  langHTML("addAssignmentButton", "addAssignment");
+  langHTML("delAssignmentButton", "deleteAssignment");
+  langHTML("creditCathy");
+  langHTML("whatNeed");
+  langHTML("desiredGrade");
+  langHTML("numPointsWorth");
+  langHTML("calcRequiredButton");
+  langHTML("changeWeightButton");
+  document.editCategory.letter.options[0].innerHTML = "-- " + langData[lang].selectLetter + " --";
+  langHTML("copyGradesInstruct");
+  langHTML("mobileCopyInstruct");
+  langHTML("pasteGradesInstruct");
+  langHTML("autoNext", "next");
+  langHTML("superAlgorithm");
+  langHTML("dontWorry");
+  langHTML("shouldContinue");
+  langHTML("rampalButton");
+  langHTML("manualButton");
+  langHTML("autoContinue", "continue");
+  document.getElementById("selClass").innerHTML = langData[lang].selClass + " <img id=\"seth_img\" src=\"SETH.JPG\">";
+  document.classes.class.options[0].innerHTML = "-- " + langData[lang].selClassOption + " --";
+  document.classes.class.options[1].innerHTML = langData[lang].addClassOption;
+  langHTML("sethClassInstruct");
+  langHTML("pointSystemInstruct");
+  langHTML("pointSystemInstruct2");
+  langHTML("nameOfClass");
+  langHTML("enterCatNames");
+  langHTML("sendMailButton", "submit");
+  langHTML("minExam");
+  langHTML("wantAtLeast");
+  document.getElementById("semesterGrade").innerHTML = langReplace("minGrade", ["$MIN", "$LETTER", "$NUMBER"], [86.50, "B+", 88.50]);
+  document.getElementById("semesterGrade").innerHTML += "<br>" + langData[lang].selectAbove;
+  langHTML("semWithExam");
+  document.getElementById("examGrade").innerHTML = langReplace("semGrade", ["$LETTER", "$NUMBER"], ["A+", 98.50]);
+  document.getElementById("examGrade").innerHTML += "<br>" + langData[lang].selectAbove;
+  langHTML("ritvikCalc");
+  langHTML("ritvikHonor");
+  langHTML("editCreditButton", "apply");
+  langHTML("deleteCreditButton", "delete");
+  langHTML("clearDataExp");
+  langHTML("clearDataButton");
+  langHTML("getCreditButton");
+  langHTML("addCreditButton", "add");
+  langHTML("modifyCreditHeading");
+}
+function languageSelect() {
+  setLanguage(document.setLanguage.language.value);
+}
+function langHTML(id, key=null) {
+  if (key == null)
+    key = id;
+  try {
+    document.getElementById(id).innerHTML = langData[lang][key];
+  } catch(err) {
+    document.getElementById(id).innerHTML = langData["en"][key];
+  }
+  if (lang == "hi") {
+    document.getElementById(id).style.direction = "rtl";
+  } else {
+    document.getElementById(id).style.direction = "";
+  }
+}
+function langReplace(key, codes, values) {
+  var str = langData[lang][key];
+  for (var i = 0; i < codes.length; i++) {
+    str = str.replace(codes[i], "" + values[i]);
+  }
+  return str;
+}
---
+++ lang/es.js
@@ -0,0 +1,104 @@
+langData["es"] = {
+  "begin": "Para empezar, crea categorías para las notas. Si la clase está basada en point system, haz una categoría llamado algo como \"Total\" y considérala 100% de tu nota.",
+  "catInstruct": "Si la clase está basada en point system, haz una categoría " +
+    "llamado algo como \"Total\" y considérala 100% de tu nota. " +
+    "Por favor no hagas ninguna categoría con un nombre que ya existe, como soy demasiado " +
+    "perezoso para asegurar que no hagas esto.",
+  "catList": "<strong>Haz clic en una categoría</strong> para añadir o quitar asignaciones y para ver la nota de esa categoría.<br><br>Categorías:<br>",
+  "catAvg": "Promedio en $CATEGORY: $NUMBER%",
+  "avg": "Nota hasta ahora en esta clase: $SCORE% (Nota de letra: $LETTER)<br>" +
+    "Unweighted GPA: $GPA<br>Suma de todas las categorías: $SUM% (Debe ser 100%)<br>",
+  "noCat": "Ninguna categoría está seleccionada actualmente",
+  "selCat": "Nota hasta ahora en $CATEGORY: $SCORE%",
+  "scoreNaN": "Tienes un denominador de cero u otro error en por lo menos una categoría. Trata de añadir algunas asignaciones.",
+  "rampalInstruct": "<b>Haz lo siguiente para <em>cada</em> asignación:</b><br>" +
+    "Haz clic en la asignación<br><img src=\"rampal/rampal1.png\"><br><img src=\"rampal/rampal2.png\"><br>Mira el <strong>\"Count as\"</strong> y introdúcelo arriba",
+  "catsFound": "$NUMBER categorías encontradas: $CATEGORIES",
+  "pointCheck": "Esta clase es point system",
+  "next": "Adelante",
+  "back": "Atrás",
+  "done": "Terminado",
+  "continue": "Continúa",
+  "help": "Ayuda",
+  "submit": "Enviar",
+  "apply": "Aplicar",
+  "add": "Añadir",
+  "delete": "Borrar",
+  "edit": "Editar las notas",
+  "newClass": "Nueva clase (automático)",
+  "notPossibleGrade": "Desafortunadamente esta nota no es posible para ti en este semestre. ¡Buena suerte para el próximo semestre!",
+  "minGrade": "Nota mínima necesaria en el examen del semestre: $MIN% (se introduce como $LETTER/$NUMBER%)",
+  "fritzExam": "Nota mínima necesaria en el examen del semestre: $MIN% (Espero que no hagas tanto Fritz en el exámen)",
+  "validData": "Por favor introduce información válida.",
+  "mailSent": "Tu respuesta fue enviada y la clase deberá ser disponible pronto. ¡Gracias!" +
+    "<br>Por ahora, vuelve y introduce las ponderaciones a mano.<br>Si la clase no esté disponible dentro de aproximadamente tres días, por favor ponte en contacto" +
+    " conmigo en bd542591@ahschool.com; quizás yo tenga problemas en entender tu solicitud",
+  "semGrade": "Nota para el semestre: $LETTER ($NUMBER%)",
+  "gpaClass": "$INDEX: $GPA ($GRADE, $LEVEL, $CREDITS créditos)",
+  "gpaSum": "<b>Suma de GPA: $SUM<br>Créditos totales: $CREDITS<br>GPA acumulativo: $GPA</b>",
+  "reqScore_0": "Debes de recibir $PTS/$TOT para sacar un $GRADE% en la clase",
+  "reqScore": "Debes de recibir $PTS/$TOT ($PERCENT%) para sacar un $GRADE% en la clase",
+  "setLang": "Idioma:&nbsp;",
+  "welcome": "Bienvenidos a la calculadora de notas. Primero tienes que asegurar que tengas " +
+    "JavaScript. Si no sabes que es eso, no te preocupes. Lo haré por ti:",
+  "jsSuccess": "Si estás leyendo este mensaje, tienes JavaScript y puedes usar esta página.",
+  "quarterGrades": "Notas del cuarto",
+  "intro": "<b>NOTA:</b> pese a mi amor para computadoras, también soy humano. Por favor ponte en contacto conmigo en bd542591@ahschool.com si encuentras un error." +
+    "<br><br>Para empezar, selecciona un cálculo:",
+  "semGradesButton": "Notas de semestre/examen",
+  "gpaCalcButton": "Calculadora de GPA",
+  "newCatButton": "Nueva categoría",
+  "catName": "Nombre:&nbsp;",
+  "catPercent": "Porcentaje:&nbsp;",
+  "createCat": "Crear categoría",
+  "addAssignment": "Añadir asignación",
+  "deleteAssignment": "Borrar asignación",
+  "editInstruct": "Introduce nota/posible. Ejemplos: 5/6.0, 8.5/9, 3/3.<br>" +
+    "Para borrar a una asignación, introduce su nota <strong>de la misma manera</strong> y haz clic en \"Borrar asignación\".<br>" +
+    "Para <strong>cambiar la nota de una asignación</strong>, añade o borra una asignación de 0 puntos (ej: 5/0).",
+  "letterGrade": "Nota de letra:&nbsp;",
+  "selectLetter": "Selecciona una nota de letra",
+  "whatNeed": "¿Qué necesito en una asignación que está yendo?",
+  "creditCathy": "Mérito a Cathy T. Quan por esta idea",
+  "desiredGrade": "Nota deseada en la clase:&nbsp;",
+  "numPointsWorth": "Número de puntos del valor de la asignación:&nbsp;",
+  "calcRequiredButton": "Calcular nota necesaria",
+  "changeWeightButton": "Cambiar ponderación",
+  "copyGradesInstruct": "Sencillamente copia tus notas de MyBackpack como se muestra en el imágen más abajo.",
+  "mobileCopyInstruct": "(Si estás usando un <strong>dispositivo móvil</strong>, incluso iPads, debes seleccionar <i>" +
+    "Assignment</i>, y después seleccionar todas las apuntes)",
+  "pasteGradesInstruct": "Ahora pégalas en la casilla más abajo y haz clic en <strong>Adelante</strong> debajo de la casilla:",
+  "superAlgorithm": "Mi algoritmo supergenial simplificó tus notas a lo siguiente (categoría, nota/posible):",
+  "dontWorry": "<strong>Si ya no se han introducido notas</strong> de una categoría, esa categoría no estará en la lista arriba." +
+    " No te preocupes, se puede añadir esa categoría a mano después del cálculo. Tampoco te preocupes si esta clase" +
+    " está basada en <strong>point system</strong> en vez de categorías.",
+  "shouldContinue": "¿Debo continuar?",
+  "rampalButton": "Haz clic aquí para la clase de Mr. Rampal",
+  "manualButton": "Introduce notas a mano",
+  "selClass": "<strong>Selecciona una clase</strong> para llenar porcentajes/ponderación automáticamente. (Mérito a <span id=\"seth\">Seth</span> Groper por esta idea)",
+  "selClassOption": "Seleccionar una clase",
+  "addClassOption": "Añadir una clase a esta lista",
+  "sethClassInstruct": "Por favor introduces un porcentaje para cada categoría <strong><i>o</i></strong> marca que esta clase usa point system:",
+  "pointSystemInstruct": "Para averiguar si una clase usa point system o categorías, mira cuánto valen pruebas, tareas, y exámenes." +
+    " Si valen aproximadamente el mismo número de puntos, es categorías. Si no, tal vez es point system.",
+  "nameOfClass": "Nombre de la clase:&nbsp;",
+  "pointSystemInstruct2": "Para <strong>point system</strong>, teclea &ldquo;point system&rdquo; para nombres y ponderaciones.",
+  "enterCatNames": "Nombres EXACTOS de las categorías como se ven en MyBackpack:&nbsp;",
+  "enterWeights": "Ponderaciones de cada categoría en el orden arriba:&nbsp;",
+  "enterNotes": "Apuntes (opcional):&nbsp;",
+  "minExam": "Nota mínima necesaria en el examen",
+  "wantAtLeast": "Quiero por lo menos:&nbsp;",
+  "selectAbove": "Selecciona apuntes arriba para calcular",
+  "semWithExam": "Nota del semestre con exámen",
+  "ritvikCalc": "Calculadora de GPA de AHS de Ritvik Teegavarapu",
+  "ritvikHonor": "Sugerido por y llamado en honor de Ritvik Teegavarapu",
+  "clearDataButton": "Borrar información de GPA",
+  "clearDataExp": "Esto te forcerá a introducir otra vez tu GPA la próxima vez que visitas esta página." +
+    " Esto NO borrará la información de la página ahora.<br><br>" +
+    "Si dejas la información en vez de borrarla, todo será guardado " +
+    "para la próxima vez que visitas esta página.",
+  "creditNum": "Crédito número&nbsp;",
+  "getCreditButton": "Ver crédito",
+  "modifyCreditHeading": "<b>Modificar/borrar notas</b>",
+  "confirm": "¿Estás seguro/a que quieres hacer esto?"
+};
