321aeb46df541dfcfe273b79e3fa313d11e1d297
crackalamoo <37599247+crackalamoo@users.noreply.github.com>
2021-04-15 23:01:20 -0400

    Updated for new hosting site

--- lang/es.js
+++ lang/es.js
@@ -114,6 +114,6 @@ langData["es"] = {
   "4scale": "Escala 4.0",
   "announcements": "Envíame un email si quisieras traducir la calculadora de notas" +
     " a otro idioma o corregir una traducción que ya existe.",
-  "footer": "Harys Dalvi<br>octubre 2019&ndash;abril 2021",
+  "footer": "Harys &ldquo;Hacer&rdquo; Dalvi<br>octubre 2019&ndash;abril 2021",
   "languages": ["inglés", "español", "portugués", "hindi", "urdu"]
 };
